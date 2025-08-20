896196aadf9becd3d97a925edeb62e0550b32012
Michael Mauderer <michael@mauderer.me>
2025-08-20 13:20:31 +0100

    Add a benchmarking script for testing parsing performance.

--- colour_cxf/tests/test_invalid_cxf_parsing.py
+++ colour_cxf/tests/test_invalid_cxf_parsing.py
@@ -9,11 +9,11 @@ proper error detection and handling of:
 """
 
 import unittest
-from lxml_asserts.testcase import LxmlTestCaseMixin
 
-import colour_cxf
-from colour_cxf import read_cxf
+from lxml_asserts.testcase import LxmlTestCaseMixin
 from xsdata.exceptions import ParserError
+
+from colour_cxf import read_cxf
 
 
 class InvalidCxfParsing(unittest.TestCase, LxmlTestCaseMixin):
@@ -23,7 +23,7 @@ class InvalidCxfParsing(unittest.TestCase, LxmlTestCaseMixin):
 
     def test_malformed_xml_structure(self) -> None:
         """Test that malformed XML structure is properly detected."""
-        
+
         # Test unclosed tag
         unclosed_tag_xml = b"""<?xml version="1.0" encoding="UTF-8"?>
 <cc:CxF xmlns:cc="http://colorexchangeformat.com/CxF3-core">
@@ -33,49 +33,49 @@ class InvalidCxfParsing(unittest.TestCase, LxmlTestCaseMixin):
         </cc:ObjectCollection>
     </cc:Resources>
 </cc:CxF>"""
-        
+
         with self.assertRaises(ParserError):
             read_cxf(unclosed_tag_xml)
-    
+
     def test_invalid_root_element(self) -> None:
         """Test that invalid root element is detected."""
-        
+
         invalid_root_xml = b"""<?xml version="1.0" encoding="UTF-8"?>
 <cc:InvalidRoot xmlns:cc="http://colorexchangeformat.com/CxF3-core">
     <cc:Resources>
     </cc:Resources>
 </cc:InvalidRoot>"""
-        
+
         with self.assertRaises(ParserError):
             read_cxf(invalid_root_xml)
-    
+
     def test_missing_namespace(self) -> None:
         """Test that missing namespace declaration is detected."""
-        
+
         missing_namespace_xml = b"""<?xml version="1.0" encoding="UTF-8"?>
 <CxF>
     <Resources>
     </Resources>
 </CxF>"""
-        
+
         with self.assertRaises(ParserError):
             read_cxf(missing_namespace_xml)
-    
+
     def test_wrong_namespace(self) -> None:
         """Test that wrong namespace is detected."""
-        
+
         wrong_namespace_xml = b"""<?xml version="1.0" encoding="UTF-8"?>
 <cc:CxF xmlns:cc="http://wrong-namespace.com/CxF3-core">
     <cc:Resources>
     </cc:Resources>
 </cc:CxF>"""
-        
+
         with self.assertRaises(ParserError):
             read_cxf(wrong_namespace_xml)
-    
+
     def test_invalid_element_structure(self) -> None:
         """Test that invalid element structure is detected."""
-        
+
         # Resources placed before FileInformation (wrong order according to schema)
         wrong_order_xml = b"""<?xml version="1.0" encoding="UTF-8"?>
 <cc:CxF xmlns:cc="http://colorexchangeformat.com/CxF3-core">
@@ -85,26 +85,26 @@ class InvalidCxfParsing(unittest.TestCase, LxmlTestCaseMixin):
         <cc:Creator>Test</cc:Creator>
     </cc:FileInformation>
 </cc:CxF>"""
-        
+
         with self.assertRaises(ParserError):
             read_cxf(wrong_order_xml)
-    
+
     def test_unknown_elements(self) -> None:
         """Test that unknown elements are detected."""
-        
+
         unknown_element_xml = b"""<?xml version="1.0" encoding="UTF-8"?>
 <cc:CxF xmlns:cc="http://colorexchangeformat.com/CxF3-core">
     <cc:Resources>
         <cc:UnknownElement>Invalid</cc:UnknownElement>
     </cc:Resources>
 </cc:CxF>"""
-        
+
         with self.assertRaises(ParserError):
             read_cxf(unknown_element_xml)
-    
+
     def test_missing_required_attributes(self) -> None:
         """Test that missing required attributes are detected."""
-        
+
         # Object without required Id attribute
         missing_id_xml = b"""<?xml version="1.0" encoding="UTF-8"?>
 <cc:CxF xmlns:cc="http://colorexchangeformat.com/CxF3-core">
@@ -117,13 +117,13 @@ class InvalidCxfParsing(unittest.TestCase, LxmlTestCaseMixin):
         </cc:ObjectCollection>
     </cc:Resources>
 </cc:CxF>"""
-        
+
         with self.assertRaises(ParserError):
             read_cxf(missing_id_xml)
-    
+
     def test_invalid_astm_table_enum(self) -> None:
         """Test that invalid ASTM table enumeration values are detected."""
-        
+
         invalid_astm_xml = b"""<?xml version="1.0" encoding="UTF-8"?>
 <cc:CxF xmlns:cc="http://colorexchangeformat.com/CxF3-core">
     <cc:Resources>
@@ -138,13 +138,13 @@ class InvalidCxfParsing(unittest.TestCase, LxmlTestCaseMixin):
         </cc:ColorSpecificationCollection>
     </cc:Resources>
 </cc:CxF>"""
-        
+
         with self.assertRaises(ParserError):
             read_cxf(invalid_astm_xml)
-    
+
     def test_invalid_sphere_type_enum(self) -> None:
         """Test that invalid sphere type enumeration values are detected."""
-        
+
         invalid_sphere_xml = b"""<?xml version="1.0" encoding="UTF-8"?>
 <cc:CxF xmlns:cc="http://colorexchangeformat.com/CxF3-core">
     <cc:Resources>
@@ -161,13 +161,13 @@ class InvalidCxfParsing(unittest.TestCase, LxmlTestCaseMixin):
         </cc:ObjectCollection>
     </cc:Resources>
 </cc:CxF>"""
-        
+
         with self.assertRaises(ParserError):
             read_cxf(invalid_sphere_xml)
-    
+
     def test_invalid_device_class_enum(self) -> None:
         """Test that invalid device class enumeration values are detected."""
-        
+
         invalid_device_xml = b"""<?xml version="1.0" encoding="UTF-8"?>
 <cc:CxF xmlns:cc="http://colorexchangeformat.com/CxF3-core">
     <cc:Resources>
@@ -186,13 +186,13 @@ class InvalidCxfParsing(unittest.TestCase, LxmlTestCaseMixin):
         </cc:ObjectCollection>
     </cc:Resources>
 </cc:CxF>"""
-        
+
         with self.assertRaises(ParserError):
             read_cxf(invalid_device_xml)
-    
+
     def test_out_of_range_rgb_values(self) -> None:
         """Test that RGB values outside valid range are detected."""
-        
+
         invalid_rgb_xml = b"""<?xml version="1.0" encoding="UTF-8"?>
 <cc:CxF xmlns:cc="http://colorexchangeformat.com/CxF3-core">
     <cc:Resources>
@@ -209,13 +209,13 @@ class InvalidCxfParsing(unittest.TestCase, LxmlTestCaseMixin):
         </cc:ObjectCollection>
     </cc:Resources>
 </cc:CxF>"""
-        
+
         with self.assertRaises(ParserError):
             read_cxf(invalid_rgb_xml)
-    
+
     def test_out_of_range_cmyk_values(self) -> None:
         """Test that CMYK values outside valid range are detected."""
-        
+
         invalid_cmyk_xml = b"""<?xml version="1.0" encoding="UTF-8"?>
 <cc:CxF xmlns:cc="http://colorexchangeformat.com/CxF3-core">
     <cc:Resources>
@@ -233,26 +233,26 @@ class InvalidCxfParsing(unittest.TestCase, LxmlTestCaseMixin):
         </cc:ObjectCollection>
     </cc:Resources>
 </cc:CxF>"""
-        
+
         with self.assertRaises(ParserError):
             read_cxf(invalid_cmyk_xml)
-    
+
     def test_invalid_datetime_format(self) -> None:
         """Test that invalid datetime format is detected."""
-        
+
         invalid_datetime_xml = b"""<?xml version="1.0" encoding="UTF-8"?>
 <cc:CxF xmlns:cc="http://colorexchangeformat.com/CxF3-core">
     <cc:FileInformation>
         <cc:CreationDate>not-a-valid-datetime</cc:CreationDate>
     </cc:FileInformation>
 </cc:CxF>"""
-        
+
         with self.assertRaises(ParserError):
             read_cxf(invalid_datetime_xml)
-    
+
     def test_duplicate_object_ids(self) -> None:
         """Test that duplicate object IDs are detected."""
-        
+
         duplicate_id_xml = b"""<?xml version="1.0" encoding="UTF-8"?>
 <cc:CxF xmlns:cc="http://colorexchangeformat.com/CxF3-core">
     <cc:Resources>
@@ -268,13 +268,13 @@ class InvalidCxfParsing(unittest.TestCase, LxmlTestCaseMixin):
         </cc:ObjectCollection>
     </cc:Resources>
 </cc:CxF>"""
-        
+
         with self.assertRaises(ParserError):
             read_cxf(duplicate_id_xml)
-    
+
     def test_invalid_color_specification_reference(self) -> None:
         """Test that invalid ColorSpecification references are detected."""
-        
+
         invalid_ref_xml = b"""<?xml version="1.0" encoding="UTF-8"?>
 <cc:CxF xmlns:cc="http://colorexchangeformat.com/CxF3-core">
     <cc:Resources>
@@ -299,13 +299,13 @@ class InvalidCxfParsing(unittest.TestCase, LxmlTestCaseMixin):
         </cc:ColorSpecificationCollection>
     </cc:Resources>
 </cc:CxF>"""
-        
+
         with self.assertRaises(ParserError):
             read_cxf(invalid_ref_xml)
-    
+
     def test_invalid_spectral_wavelength_range(self) -> None:
         """Test that invalid spectral wavelength ranges are detected."""
-        
+
         invalid_spectrum_xml = b"""<?xml version="1.0" encoding="UTF-8"?>
 <cc:CxF xmlns:cc="http://colorexchangeformat.com/CxF3-core">
     <cc:Resources>
@@ -320,13 +320,13 @@ class InvalidCxfParsing(unittest.TestCase, LxmlTestCaseMixin):
         </cc:ObjectCollection>
     </cc:Resources>
 </cc:CxF>"""
-        
+
         with self.assertRaises(ParserError):
             read_cxf(invalid_spectrum_xml)
-    
+
     def test_negative_spectral_increment(self) -> None:
         """Test that negative spectral increment is detected."""
-        
+
         negative_increment_xml = b"""<?xml version="1.0" encoding="UTF-8"?>
 <cc:CxF xmlns:cc="http://colorexchangeformat.com/CxF3-core">
     <cc:Resources>
@@ -341,13 +341,13 @@ class InvalidCxfParsing(unittest.TestCase, LxmlTestCaseMixin):
         </cc:ObjectCollection>
     </cc:Resources>
 </cc:CxF>"""
-        
+
         with self.assertRaises(ParserError):
             read_cxf(negative_increment_xml)
-    
+
     def test_invalid_xml_encoding(self) -> None:
         """Test that invalid XML encoding is detected."""
-        
+
         # Invalid characters in XML
         invalid_encoding_xml = b"""<?xml version="1.0" encoding="UTF-8"?>
 <cc:CxF xmlns:cc="http://colorexchangeformat.com/CxF3-core">
@@ -355,13 +355,13 @@ class InvalidCxfParsing(unittest.TestCase, LxmlTestCaseMixin):
         <cc:Creator>\x00InvalidChar</cc:Creator>
     </cc:FileInformation>
 </cc:CxF>"""
-        
+
         with self.assertRaises(ParserError):
             read_cxf(invalid_encoding_xml)
-    
+
     def test_empty_required_fields(self) -> None:
         """Test that empty required fields are detected."""
-        
+
         empty_name_xml = b"""<?xml version="1.0" encoding="UTF-8"?>
 <cc:CxF xmlns:cc="http://colorexchangeformat.com/CxF3-core">
     <cc:Resources>
@@ -373,7 +373,7 @@ class InvalidCxfParsing(unittest.TestCase, LxmlTestCaseMixin):
         </cc:ObjectCollection>
     </cc:Resources>
 </cc:CxF>"""
-        
+
         with self.assertRaises(ParserError):
             read_cxf(empty_name_xml)
 
